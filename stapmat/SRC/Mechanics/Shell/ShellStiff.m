33f4bbbe27ca0203837cbfd4ef8e90ef65bce2ea
Zs <2585978798@qq.com>
2023-12-14 13:56:04 +0800

    created

---
+++ stapmat/SRC/Mechanics/Shell/ShellStiff.m
@@ -0,0 +1,120 @@
+%* *****************************************************************
+%* - Function of STAPMAT in stiffness phase                        *
+%*                                                                 *
+%* - Purpose:                                                      *
+%*     Compute the stiffness matrix of quad                        *
+%*                                                                 *
+%* - Call procedures:                                              *
+%*     ShellStiff.m - InitQuad()                                   *
+%*     ./ReadShell.m - ReadQuad()                                  *
+%*     SRC/Mechanics/Addres.m - Addres()                           *
+%*                                                                 *
+%* - Called by :                                                   *
+%*     SRC/Mechanics/GetStiff.m                                    *
+%*                                                                 *
+%* - Programmed by:                                                *
+%*     Zhang  Chen                                                 *
+%* *****************************************************************
+
+
+
+function ShellStiff()
+
+% Init variables of the element
+InitShell();
+
+% Read Material and Elements
+ReadShell();
+
+fprintf('Solution phase ...\n\n');
+
+% calculate addresses of diagonal elements
+Addres();
+
+% Data check Or Solve
+global cdata;
+if (cdata.MODEX == 0) 
+    cdata.TIM(3,:) = clock;
+    cdata.TIM(4,:) = clock;
+    cdata.TIM(5,:) = clock;
+    return; 
+end
+
+% Assemble structure stiffness matrix
+Assemble();
+
+
+end
+
+
+% ----------------------- Functions -----------------------------------
+
+% Init parameters of quad element
+
+function InitShell()
+global sdata;
+sdata.NNODE = 4;
+sdata.NDOF = 3;   %自由度：θx，θy，w
+
+end
+
+% Assemble structure stiffness matrix
+function Assemble()
+global sdata;
+global cdata;
+K = zeros(12, 12, 'double');
+sdata.STIFF = zeros(sdata.NWK, 1, 'double');
+
+NUME = sdata.NUME; MATP = sdata.MATP; XYZ = sdata.XYZ; 
+E = sdata.E; nu = sdata.nu; LM = sdata.LM; thick=sdata.thick;%厚度
+for N = 1:NUME
+    MTYPE = MATP(N);
+    
+%   compute the matrix D
+    D_plane = E(MTYPE)/(1-nu(MTYPE)^2)*[1 nu(MTYPE) 0;nu(MTYPE) 1 0;0 0 (1-nu(MTYPE))/2]; %平面应力矩阵Dplane
+    k=5/6; %剪切校正因子，通常为5/6
+    D_shear=k*E(MTYPE)/(2*(1+nu(MTYPE)))*[1 0;0 1]; %横向剪切矩阵Dshear
+
+
+%   compute the matrix B & K 
+    K = zeros(12,12);
+    P = [XYZ(1, N) XYZ(2, N);XYZ(4, N) XYZ(5, N);
+        XYZ(7, N) XYZ(8, N);XYZ(10, N) XYZ(11, N)];   %四节点坐标
+    s = [-sqrt(3)/3 sqrt(3)/3];
+    t = [-sqrt(3)/3 sqrt(3)/3];   %高斯积分点
+    for i = 1:2
+        for j = 1:2        %循环遍历所有高斯积分点
+            N_st = 0.25*[t(j)-1 1-t(j) 1+t(j) -1-t(j);
+                s(i)-1 -1-s(i) 1+s(i) 1-s(i)];         %形函数的导数
+            J = N_st*P;              %计算雅可比矩阵J
+            detJ = det(J);           % 雅可比行列式
+            N_xy = J\N_st;           %计算局部到全局坐标的映射
+            B_plane = [N_xy(1,1) 0 N_xy(1,2) 0 N_xy(1,3) 0 N_xy(1,4) 0;
+                0 N_xy(2,1) 0 N_xy(2,2) 0 N_xy(2,3) 0 N_xy(2,4);
+                N_xy(2,1) N_xy(1,1) N_xy(2,2) N_xy(1,2) N_xy(2,3) N_xy(1,3) N_xy(2,4) N_xy(1,4)];
+            B_shear = [N_xy(1,1) 0  -thick/2 N_xy(1,2) 0 -thick/2 N_xy(1,3) 0 -thick/2 N_xy(1,4) 0 -thick/2;
+                        0 N_xy(2,1) -thick/2 0 N_xy(2,2) -thick/2 0 N_xy(2,3) -thick/2 0 N_xy(2,4) -thick/2];
+            K_plane = B_plane' * D_plane * B_plane;
+            K_plane_expanded = zeros(12, 12);
+            K_plane_expanded(1:8, 1:8) = K_plane;  % 将 8x8 矩阵扩展到 12x12
+            % 计算 B_shear 的刚度贡献
+            K_shear = B_shear' * D_shear * B_shear;  % 假设这是一个 12x12 矩阵
+            % 组合两个刚度矩阵
+            K_combined = K_plane_expanded + K_shear;
+            K = K + K_combined*detJ*thick;
+        end
+    end
+    
+%   SRC/Mechanics/ADDBAN.m
+    ADDBAN(K, LM(:, N));
+    
+end
+
+% The third time stamp
+cdata.TIM(3, :) = clock;
+
+end
+
+
+
+
---
+++ stapmat/SRC/Mechanics/Shell/ShellStress.m
@@ -0,0 +1,84 @@
+%* *****************************************************************
+%* - Function of STAPMAT in solver phase                           *
+%*                                                                 *
+%* - Purpose:                                                      *
+%*     To calculate stresses                                       *
+%*                                                                 *
+%* - Call procedures: None                                         *
+%*                                                                 *
+%* - Called by :                                                   *
+%*     SRC/Solver/GetStress.m                                      *
+%*                                                                 *
+%* - Programmed by:                                                *
+%*      Zhang Chen                                                 *
+%* *****************************************************************
+
+function QuadStress(NUM, NG)
+
+% Get global data
+global cdata;
+global sdata;
+
+IOUT = cdata.IOUT;
+NUME = sdata.NUME; MATP = sdata.MATP; XYZ = sdata.XYZ;
+E = sdata.E; nu = sdata.nu; LM = sdata.LM;thick=sdata.thick;
+U = sdata.DIS(:, NUM);
+
+fprintf(IOUT, ['\n\n  S T R E S S  C A L C U L A T I O N S  F O R  ' ...
+    'E L E M E N T  G R O U P %4d\n\n' ... 
+    '       ELEMENT             SIGMAxx         SIGMAyy         SIGMAxy         TAUxz          TAUyz\n' ...
+    '       NUMBER\n'], NG);
+
+for N = 1:NUME
+    MTYPE = MATP(N);
+   
+%   displacement
+    for i = 1:12
+        if LM(i, N) ~= 0
+            u(i) = U(LM(i, N));
+        else
+            u(i) = 0;
+        end
+    end
+    u
+    d = [u(1);u(2);u(3);u(4);u(5);u(6);u(7);u(8);u(9);u(10);u(11);u(12)];
+    
+    B1_plane=0;
+    B1_shear=0;
+%   compute the matrix D
+    D_plane = E(MTYPE)/(1-nu(MTYPE)^2)*[1 nu(MTYPE) 0;nu(MTYPE) 1 0;0 0 (1-nu(MTYPE))/2]; %平面应力矩阵Dplane
+    k=5/6; %剪切校正因子，通常为5/6
+    D_shear=k*E(MTYPE)/(2*(1+nu(MTYPE)))*[1 0;0 1]; %横向剪切矩阵Dshear
+%   compute the matrix B & K 
+    K = zeros(12,12);
+    P = [XYZ(1, N) XYZ(2, N);XYZ(4, N) XYZ(5, N);
+        XYZ(7, N) XYZ(8, N);XYZ(10, N) XYZ(11, N)];   %四节点坐标
+    s = [-sqrt(3)/3 sqrt(3)/3];
+    t = [-sqrt(3)/3 sqrt(3)/3];   %高斯积分点
+    for i = 1:2
+        for j = 1:2        %循环遍历所有高斯积分点
+            N_st = 0.25*[t(j)-1 1-t(j) 1+t(j) -1-t(j);
+                s(i)-1 -1-s(i) 1+s(i) 1-s(i)];         %形函数的导数
+            J = N_st*P;              %计算雅可比矩阵J
+            detJ = det(J);           % 雅可比行列式
+            N_xy = J\N_st;           %计算局部到全局坐标的映射
+            B_plane = [N_xy(1,1) 0 N_xy(1,2) 0 N_xy(1,3) 0 N_xy(1,4) 0;
+                0 N_xy(2,1) 0 N_xy(2,2) 0 N_xy(2,3) 0 N_xy(2,4);
+                N_xy(2,1) N_xy(1,1) N_xy(2,2) N_xy(1,2) N_xy(2,3) N_xy(1,3) N_xy(2,4) N_xy(1,4)];
+            B_shear = [N_xy(1,1) 0  -thick/2 N_xy(1,2) 0 -thick/2 N_xy(1,3) 0 -thick/2 N_xy(1,4) 0 -thick/2;
+                        0 N_xy(2,1) -thick/2 0 N_xy(2,2) -thick/2 0 N_xy(2,3) -thick/2 0 N_xy(2,4) -thick/2]
+            B1_plane=0.25*B_plane+B1_plane;
+            B1_shear=0.25*B_shear+B1_shear;
+        end
+    end
+    B1_plane_extended = zeros(3, 12);
+    B1_plane_extended(:, 1:8) = B1_plane;
+    eps_plane = B1_plane_extended*d;
+    eps_shear = B1_shear*d;
+    sigma_plane = D_plane*eps_plane; %σx、σy、σxy
+    sigma_shear = D_shear*eps_shear; %τxz、τyz
+    
+    fprintf(IOUT, ' %10d     %13.6e     %13.6e    %13.6e     %13.6e     %13.6e\n', N, sigma_plane(1), sigma_plane(2) ,sigma_plane(3), sigma_shear(1), sigma_shear(2));
+end
+
+end
